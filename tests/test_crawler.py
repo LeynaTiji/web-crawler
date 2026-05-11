587342e851e995d8881030a683b7d3b0704dd891
LeynaTiji <leynatiji@icloud.com>
2026-05-11 15:29:04 +0100

    added test for get new links function

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -2,14 +2,19 @@ import unittest
 from unittest.mock import MagicMock, patch
 from bs4 import BeautifulSoup
  
-from crawler import extract_links, extract_text, get_page, crawl
+from crawler import get_new_link, extract_text, get_page, crawl
 
 #helper function, returns a beautiful soup oject
 def make_soup(html: str) -> BeautifulSoup:
     return BeautifulSoup(html, "html.parser")
 
-
 class TestGetNewLinks(unittest.TestCase):
+
+    def test_returns_internal_links(self):
+        soup = make_soup('<a href="/page/2/">next</a>')
+        links = get_new_link(soup, "https://quotes.toscrape.com")
+        self.assertIn("https://quotes.toscrape.com/page/2", links)
+
 
 # class TestExtractText(unittest.TestCase):
 
