ee4659d1b58edfb8ec9cb90173707b5ff37876e6
LeynaTiji <leynatiji@icloud.com>
2026-05-12 14:32:42 +0100

    tests for findcommand

--- tests/test_search.py
+++ tests/test_search.py
@@ -27,3 +27,30 @@ class TestFind(unittest.TestCase):
     def test_single_word_not_in_index_returns_empty(self):
         results = find_query("interesting", SAMPLE_INDEX)
         self.assertEqual(results, [])
+
+    def test_multi_word(self):
+        results = find_query("good friends", SAMPLE_INDEX)
+        urls = [url for url, _ in results]
+        self.assertEqual(urls, ["https://example.com/page1"])
+
+    def test_multi_word_with_no_common_pages(self):
+        results = find_query("morning friends", SAMPLE_INDEX)
+        self.assertEqual(results, [])
+
+    def test_empty_query_returns_empty(self):
+        results = find_query("", SAMPLE_INDEX)
+        self.assertEqual(results, [])
+
+    def test_case_insensitive(self):
+        results_lower = find_query("good", SAMPLE_INDEX)
+        results_upper = find_query("GOOD", SAMPLE_INDEX)
+        self.assertEqual(results_lower, results_upper)
+
+    def test_empty_index_returns_empty(self):
+        results = find_query("good", {})
+        self.assertEqual(results, [])
+ 
+    def test_returns_score_with_url(self):
+        results = find_query("good", SAMPLE_INDEX)
+        self.assertIsInstance(results[0], tuple)
+        self.assertEqual(len(results[0]), 2)
